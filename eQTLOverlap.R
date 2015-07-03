e0e00a877b63a38ef6dd8a7ab8fbb9f3e761acaf
ckagan <courtneykagan@gmail.com>
2015-07-03 17:16:53 -0500

    Update
    Checked pearson correclation, added SE and Beta script to GETx,, comment
    out original GTEx overlap

--- eQTLOverlap.R
+++ eQTLOverlap.R
@@ -1,143 +1,146 @@
-setwd("C:/Users/Courtney/Dropbox/LCL-iPSC/GEMMA eQTLs/GEMMATest/Enrichment")
-liver = read.table('Adipose.eqtl.txt', header=T)
-art = read.table('Artery_Aorta.eqtl.txt', header=T)
-tib = read.table('Artery_Tibial.eqtl.txt', header=T)
-ipsc = read.table('PC13_eQTLResults.corrected.chosen.txt', header=T)
-CAGETSS = read.table('ENSG_CAGETSS.txt', header=T)
-eso = read.table('Esophagus_Mucosa.eqtl.txt', header=T)
-eso2 = read.table('Esophagus_Muscularis.eqtl.txt', header=T)
-heart = read.table('Heart_Left_Ventricle.eqtl.txt', header=T)
-lung = read.table('Lung.eqtl.txt', header=T)
-musc = read.table('Muscle_Skeletal.eqtl.txt', header=T)
-nerve = read.table('Nerve_Tibial.eqtl.txt', header=T)
-skin = read.table('Skin_Sun_Exposed_Lower_leg.eqtl.txt', header=T)
-stom = read.table('Stomach.eqtl.txt', header=T)
-thy = read.table('Thyroid.eqtl.txt', header=T)
-wb = read.table('Whole_Blood.eqtl.txt', header=T)
-ipsc2 = as.data.frame(ipsc$V1)
-colnames(ipsc2) = c("Gen_ID")
-pluri = read.table('Plurigenes.txt', header=T)
+##Original GTEx analysis commented out
+# setwd("C:/Users/Courtney/Dropbox/LCL-iPSC/GEMMA eQTLs/GEMMATest/Enrichment")
+# liver = read.table('Adipose.eqtl.txt', header=T)
+# art = read.table('Artery_Aorta.eqtl.txt', header=T)
+# tib = read.table('Artery_Tibial.eqtl.txt', header=T)
+# ipsc = read.table('PC13_eQTLResults.corrected.chosen.txt', header=T)
+# CAGETSS = read.table('ENSG_CAGETSS.txt', header=T)
+# eso = read.table('Esophagus_Mucosa.eqtl.txt', header=T)
+# eso2 = read.table('Esophagus_Muscularis.eqtl.txt', header=T)
+# heart = read.table('Heart_Left_Ventricle.eqtl.txt', header=T)
+# lung = read.table('Lung.eqtl.txt', header=T)
+# musc = read.table('Muscle_Skeletal.eqtl.txt', header=T)
+# nerve = read.table('Nerve_Tibial.eqtl.txt', header=T)
+# skin = read.table('Skin_Sun_Exposed_Lower_leg.eqtl.txt', header=T)
+# stom = read.table('Stomach.eqtl.txt', header=T)
+# thy = read.table('Thyroid.eqtl.txt', header=T)
+# wb = read.table('Whole_Blood.eqtl.txt', header=T)
+# ipsc2 = as.data.frame(ipsc$V1)
+# colnames(ipsc2) = c("Gen_ID")
+# pluri = read.table('Plurigenes.txt', header=T)
+# 
+# ipsc.genes = read.table('ENSGList.allgenes.Ordered.txt')
+# ipsc.genes.tested = read.table('iPSCGenesTested.txt')
+# LCL.genes.tested = read.table('LCLGenesTested.txt')
+# LCL.genes = read.table('LCLExpressedGenes.txt')
+# colnames(ipsc.genes) = c("Gen_ID")
+# 
+# all=c()
+# all = c(as.character(CAGETSS$ENSG),as.character(liver$Gen_ID), as.character(ipsc2$Gen_ID),as.character(art$Gen_ID),as.character(tib$Gen_ID))
+# all = unique(all)
+# 
+# univ <- data.frame(all)
+# library(VennDiagram)
+# names(univ) <- "probes"
+# 
+# make.venn.quad <- function(geneset1, geneset2, geneset3, geneset4, geneset1.label, geneset2.label, geneset3.label, geneset4.label, univ){
+#   univ$g1 <- univ$probes %in% geneset1
+#   univ$g2 <- univ$probes %in% geneset2
+#   univ$g3 <- univ$probes %in% geneset3 
+#   univ$g4 <- univ$probes %in% geneset4 
+#   #pdf(file=paste(prefix, ".pdf", sep=""), width=7, height=7)
+#   venn.placeholder <- draw.quad.venn(length(geneset1),length(geneset2), length(geneset3), length(geneset4), dim(univ[univ$g1 == T & univ$g2 == T , ])[1], dim(univ[univ$g1 == T & univ$g3 == T , ])[1], dim(univ[univ$g1 == T & univ$g4 == T , ])[1], dim(univ[univ$g2 == T & univ$g3 == T , ])[1], dim(univ[univ$g2 == T & univ$g4 == T , ])[1], dim(univ[univ$g3 == T & univ$g4 == T , ])[1], dim(univ[univ$g1 == T & univ$g2 == T & univ$g3 == T , ])[1], dim(univ[univ$g1 == T & univ$g2 == T & univ$g4 == T , ])[1], dim(univ[univ$g1 == T & univ$g3 == T & univ$g4 == T , ])[1], dim(univ[univ$g2 == T & univ$g3 == T & univ$g4 == T , ])[1],  dim(univ[univ$g1 == T & univ$g2 == T & univ$g3 == T & univ$g4 == T , ])[1], c(geneset1.label, geneset2.label, geneset3.label, geneset4.label), fill=c("goldenrod", "plum4", "steelblue3", "darkolivegreen3"), alpha=c(0.5, 0.5, 0.5, 0.5),col=NA, euler.d=T)
+#   complement.size <- dim(univ[univ$g1 == F & univ$g2 == F & univ$g3 == F & univ$g4 == F , ])[1]
+#   grid.text(paste(complement.size, " not DE in any", sep=""), x=0.2, y=0.08)
+#   #dev.off()
+# }
+# 
+# #dev.off()
+# # Make venn of full
+# pdf(file = "VennDiagram_DE_FDR5.pdf")
+# make.venn.quad(as.character(liver$Gen_ID), as.character(ipsc2$Gen_ID),as.character(art$Gen_ID),as.character(tib$Gen_ID),"liver","ipsc","art","tib" ,univ)
+# dev.off()
+# 
+# res=matrix(data=NA, nrow=15418, ncol=13)
+# labs = c("liver", "art", "tib", "eso","eso2","heart","lung","musc","nerve","skin","stom","wb","ipsc2")
+# tissues = c(liver, art, tib, eso,eso2,heart,lung,musc,nerve,skin,stom,wb,ipsc2)
+#   for (i in 1:length(tissues)){
+#     m = tissues[i]
+#     test = univ$probes %in% m$Gen_ID
+#     res[,i] = test
+#   }
+# res = as.data.frame(res)
+# dim(univ[res$V1 == T & res$V2 == T & res$V3 == T & res$V4 == T & res$V5 == T & res$V6 == T & res$V7 == T & res$V8 == T & res$V9 == T & res$V10 == T & res$V11 == T & res$V12 == T & res$V13 == T, ])[1]
+# 
+# 
+# 
+# labs = c("liver", "art", "tib", "eso","eso2","heart","lung","musc","nerve","skin","stom","wb","ipsc2")
+# tissues = c(liver, art, tib, eso,eso2,heart,lung,musc,nerve,skin,stom,wb,ipsc2)
+# for (k in 1:length(tissues)){
+#   table= matrix(data=NA, nrow=13, ncol=3)
+#   props=c()
+#   for (i in 1:length(tissues)){
+#     j = tissues[i]
+#     m = tissues[k]
+#     test = m$Gen_ID %in% j$Gen_ID
+#     tr = sum(test == T)
+#     total = length(j$Gen_ID)
+#     prop = tr/total
+#     props[i]=prop
+#     table[i,1]=total
+#     table[i,2] =tr
+#   }
+#   plot(1:12, props[1:12], main = paste("Tissue Overlap with", labs[k]))
+# }
+# 
+# 
+# #Finding how many from their data is tested in mine and getting a proportion only for that
+# props.ipsconly=c()
+# for (i in 1:12){
+#   j = tissues[i]
+#   m = ipsc.genes
+#   subs = match(j$Gen_ID,m$Gen_ID)
+#   subs2 = na.omit(subs)
+#   subset = as.data.frame(ipsc.genes[subs2,])
+#   colnames(subset) = c("Gen_ID")
+#   test = ipsc2$Gen_ID %in% subset$Gen_ID
+#   tr = sum(test == T)
+#   total = length(subset$Gen_ID)
+#   table[i,2] = total
+#   table[i,3] = tr
+#   prop = tr/total
+#   props.ipsconly[i]=prop
+# }
+# counts = table(props.ipsconly)
+# plot(1:12, props.ipsconly, main = "Tissue Overlap with iPSCs - Overlapping genes only")
+# 
+# 
+# subset.size= matrix(data=NA, nrow=12, ncol=2)
+# for (i in 1:12){
+#   j = tissues[i]
+#   m = ipsc.genes
+#   subs = match(j$Gen_ID,m$Gen_ID)
+#   subs2 = na.omit(subs)
+#   subset = as.data.frame(ipsc.genes[subs2,])
+#   colnames(subset) = c("Gen_ID")
+#   assign(paste("subset.",labs[i],sep=""),subset)
+#   total = length(subset$Gen_ID)
+#   subset.size[i,1] = labs[i]
+#   subset.size[i,2] = total
+# }
+# 
+# tissues.subset = c(subset.liver, subset.art, subset.tib, subset.eso,subset.eso2,subset.heart,subset.lung,subset.musc,subset.nerve,subset.skin,subset.stom,subset.wb,ipsc2)
+# ##1000 perms per tissue type
+# perm.output= matrix(data=NA, nrow=12, ncol=100000)
+# for(i in 1:100000) {
+# perm = sample(1:10250, 1028, replace=F)
+# perm.data = as.data.frame(ipsc.genes[perm,])
+# colnames(perm.data)=c("Gen_ID")
+# for (k in 1:12){
+#   j = tissues.subset[k] 
+#   m = perm.data
+#   test = m$Gen_ID %in% j$Gen_ID
+#   tr = sum(test == T)
+#   perm.output[k,i] = tr
+# }
+# }
+# 
+# for(i in 1:12){
+# print(sum(perm.output[i,] > table[i,3]))
+# }
 
-ipsc.genes = read.table('ENSGList.allgenes.Ordered.txt')
-ipsc.genes.tested = read.table('iPSCGenesTested.txt')
-LCL.genes.tested = read.table('LCLGenesTested.txt')
-LCL.genes = read.table('LCLExpressedGenes.txt')
-colnames(ipsc.genes) = c("Gen_ID")
 
-all=c()
-all = c(as.character(CAGETSS$ENSG),as.character(liver$Gen_ID), as.character(ipsc2$Gen_ID),as.character(art$Gen_ID),as.character(tib$Gen_ID))
-all = unique(all)
-
-univ <- data.frame(all)
-library(VennDiagram)
-names(univ) <- "probes"
-
-make.venn.quad <- function(geneset1, geneset2, geneset3, geneset4, geneset1.label, geneset2.label, geneset3.label, geneset4.label, univ){
-  univ$g1 <- univ$probes %in% geneset1
-  univ$g2 <- univ$probes %in% geneset2
-  univ$g3 <- univ$probes %in% geneset3 
-  univ$g4 <- univ$probes %in% geneset4 
-  #pdf(file=paste(prefix, ".pdf", sep=""), width=7, height=7)
-  venn.placeholder <- draw.quad.venn(length(geneset1),length(geneset2), length(geneset3), length(geneset4), dim(univ[univ$g1 == T & univ$g2 == T , ])[1], dim(univ[univ$g1 == T & univ$g3 == T , ])[1], dim(univ[univ$g1 == T & univ$g4 == T , ])[1], dim(univ[univ$g2 == T & univ$g3 == T , ])[1], dim(univ[univ$g2 == T & univ$g4 == T , ])[1], dim(univ[univ$g3 == T & univ$g4 == T , ])[1], dim(univ[univ$g1 == T & univ$g2 == T & univ$g3 == T , ])[1], dim(univ[univ$g1 == T & univ$g2 == T & univ$g4 == T , ])[1], dim(univ[univ$g1 == T & univ$g3 == T & univ$g4 == T , ])[1], dim(univ[univ$g2 == T & univ$g3 == T & univ$g4 == T , ])[1],  dim(univ[univ$g1 == T & univ$g2 == T & univ$g3 == T & univ$g4 == T , ])[1], c(geneset1.label, geneset2.label, geneset3.label, geneset4.label), fill=c("goldenrod", "plum4", "steelblue3", "darkolivegreen3"), alpha=c(0.5, 0.5, 0.5, 0.5),col=NA, euler.d=T)
-  complement.size <- dim(univ[univ$g1 == F & univ$g2 == F & univ$g3 == F & univ$g4 == F , ])[1]
-  grid.text(paste(complement.size, " not DE in any", sep=""), x=0.2, y=0.08)
-  #dev.off()
-}
-
-#dev.off()
-# Make venn of full
-pdf(file = "VennDiagram_DE_FDR5.pdf")
-make.venn.quad(as.character(liver$Gen_ID), as.character(ipsc2$Gen_ID),as.character(art$Gen_ID),as.character(tib$Gen_ID),"liver","ipsc","art","tib" ,univ)
-dev.off()
-
-res=matrix(data=NA, nrow=15418, ncol=13)
-labs = c("liver", "art", "tib", "eso","eso2","heart","lung","musc","nerve","skin","stom","wb","ipsc2")
-tissues = c(liver, art, tib, eso,eso2,heart,lung,musc,nerve,skin,stom,wb,ipsc2)
-  for (i in 1:length(tissues)){
-    m = tissues[i]
-    test = univ$probes %in% m$Gen_ID
-    res[,i] = test
-  }
-res = as.data.frame(res)
-dim(univ[res$V1 == T & res$V2 == T & res$V3 == T & res$V4 == T & res$V5 == T & res$V6 == T & res$V7 == T & res$V8 == T & res$V9 == T & res$V10 == T & res$V11 == T & res$V12 == T & res$V13 == T, ])[1]
-
-
-
-labs = c("liver", "art", "tib", "eso","eso2","heart","lung","musc","nerve","skin","stom","wb","ipsc2")
-tissues = c(liver, art, tib, eso,eso2,heart,lung,musc,nerve,skin,stom,wb,ipsc2)
-for (k in 1:length(tissues)){
-  table= matrix(data=NA, nrow=13, ncol=3)
-  props=c()
-  for (i in 1:length(tissues)){
-    j = tissues[i]
-    m = tissues[k]
-    test = m$Gen_ID %in% j$Gen_ID
-    tr = sum(test == T)
-    total = length(j$Gen_ID)
-    prop = tr/total
-    props[i]=prop
-    table[i,1]=total
-    table[i,2] =tr
-  }
-  plot(1:12, props[1:12], main = paste("Tissue Overlap with", labs[k]))
-}
-
-
-#Finding how many from their data is tested in mine and getting a proportion only for that
-props.ipsconly=c()
-for (i in 1:12){
-  j = tissues[i]
-  m = ipsc.genes
-  subs = match(j$Gen_ID,m$Gen_ID)
-  subs2 = na.omit(subs)
-  subset = as.data.frame(ipsc.genes[subs2,])
-  colnames(subset) = c("Gen_ID")
-  test = ipsc2$Gen_ID %in% subset$Gen_ID
-  tr = sum(test == T)
-  total = length(subset$Gen_ID)
-  table[i,2] = total
-  table[i,3] = tr
-  prop = tr/total
-  props.ipsconly[i]=prop
-}
-counts = table(props.ipsconly)
-plot(1:12, props.ipsconly, main = "Tissue Overlap with iPSCs - Overlapping genes only")
-
-
-subset.size= matrix(data=NA, nrow=12, ncol=2)
-for (i in 1:12){
-  j = tissues[i]
-  m = ipsc.genes
-  subs = match(j$Gen_ID,m$Gen_ID)
-  subs2 = na.omit(subs)
-  subset = as.data.frame(ipsc.genes[subs2,])
-  colnames(subset) = c("Gen_ID")
-  assign(paste("subset.",labs[i],sep=""),subset)
-  total = length(subset$Gen_ID)
-  subset.size[i,1] = labs[i]
-  subset.size[i,2] = total
-}
-
-tissues.subset = c(subset.liver, subset.art, subset.tib, subset.eso,subset.eso2,subset.heart,subset.lung,subset.musc,subset.nerve,subset.skin,subset.stom,subset.wb,ipsc2)
-##1000 perms per tissue type
-perm.output= matrix(data=NA, nrow=12, ncol=100000)
-for(i in 1:100000) {
-perm = sample(1:10250, 1028, replace=F)
-perm.data = as.data.frame(ipsc.genes[perm,])
-colnames(perm.data)=c("Gen_ID")
-for (k in 1:12){
-  j = tissues.subset[k] 
-  m = perm.data
-  test = m$Gen_ID %in% j$Gen_ID
-  tr = sum(test == T)
-  perm.output[k,i] = tr
-}
-}
-
-for(i in 1:12){
-print(sum(perm.output[i,] > table[i,3]))
-}
-
+##With LCL subset
 setwd("C:/Users/Courtney/Dropbox/LCL-iPSC/GEMMA eQTLs/GEMMATest/Enrichment")
 ipsc = read.table('iPSC.PC13.gemma.chosen.txt', header=F)
 LCL = read.table('LCL.PC8.gemma.chosen.txt', header=F)
@@ -208,6 +211,7 @@ LCL.both = LCL[tested.both,]
 tested.both = which(ipsc$V1 %in% LCL$V1)
 ipsc.both = ipsc[tested.both,]
 #8,887 shared genes
+write.table(ipsc.both$V1, 'BothTestedBackground.txt', col.names=F, row.names=F, quote=F, sep='\t')
 
 perm.both.output= matrix(data=NA, nrow=100000, ncol=1)
 for(i in 1:100000) {
@@ -367,6 +371,8 @@ shared.2 = which(LCL.eqtls$V1 %in% ipsc.eqtls$V1)
 LCL.only.eqtls = LCL.eqtls[-shared.2,]
 #2034 LCL specific
 
+tested.both.genes = which(ipsc.genes.tested$V1 %in% LCL.genes.tested$V1)
+
 tested.both = which(ipsc.only.eqtls$V1 %in% LCL.genes.tested$V1)
 ipsc.only.tested.eqtls = ipsc.only.eqtls[tested.both,]
 #666 iPSC speific and tested in LCLs
@@ -382,6 +388,10 @@ expr.both = which(LCL.only.tested.eqtls$V1 %in% ipsc.genes$V1)
 #1071 LCL specific and expressed in iPSCs
 
 LCL.only.tested.expressed.eqtls = LCL.only.tested.eqtls[expr.both,]
+
+iPSC.noteQTL = which(ipsc.both$V1 %in% ipsc.only.tested.expressed.eqtls$V1)
+ipsc.not = ipsc.both[-iPSC.noteQTL,]
+write.table(ipsc.not$V1, 'NoniPSC.eQTLs.txt',quote=F, row.names=F, sep='\t', col.names=F)
 
 ##Do permutation for overall overlap
 #First subset only genes that were tested and expressed in both tissues
@@ -430,6 +440,11 @@ colnames(shared.eqtls) = c("ENSG", "Variant", "p", "Bonf", "Beta", "Se","Qval")
 shared.gtex = merge(shared.eqtls, gtex, by.x = c("ENSG", "Variant"), by.y = c("Gene", "SNP"))
 shared.esQTL = merge(shared.eqtls, esQTL, by.x = c("ENSG"), by.y = c("Gene"))
 #3 shared eQTLs overlap with GTEx list
+
+##Write out data tissue specific eQTLs
+write.table(LCL.eqtls.f, 'LCL_specific_eQTLs_full.txt', quote=F, row.names=F, sep='\t')
+write.table(ipsc.eqtls.f, 'iPSC_specific_eQTLs_full.txt', quote=F, row.names=F, sep='\t')
+write.table()
 
 ##Permute esQTL overlap
 perm.esQTL.LCL= matrix(data=NA, nrow=100000, ncol=1)
--- eQTLPlots.R
+++ eQTLPlots.R
@@ -25,42 +25,87 @@ rownames(PC1_13.residual) = rownames(exprs.ordered)
 colnames(PC1_13.residual) = colnames(exprs.ordered)
 for (i in 1:nrow(exprs.ordered)) {
   model= lm(exprs.ordered[i,]~ x.pca$x[,1:13])
-  PC1_13.residual[i,] = resid(model)
+  PC1_13.residual[i,] = resid(model) #+model$coefficients[1]
 }
 
 exprs.o.t = t(PC1_13.residual)
 
+#library(gplots)
+#heatmap.2(cor(as.matrix(PC1_13.residual), use = "complete"), margins=c(7,8),trace="none",main="Gene Expression Correlation", key=T, keysize=1.5,density.info="none",cexCol=0.9)
+
+##Load in iPSC geno
 geno.i = read.table('C:/Users/Courtney/Dropbox/LCL-iPSC/GEMMA eQTLs/Final Data/ipsc.eqtlgenotypes.raw', header=T, as.is=T, sep='\t', row.names=1)
 geno.i.t = t(geno.i)
 
-library(beeswarm)
 
-pdf("Opposite_Direction_eQTLs.pdf")
-eqtl.loc = as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000164048")])
-eqtl.geno = as.matrix(geno.i.t[which(rownames(geno.i.t) == "rs12495221_C"),])
-boxplot(eqtl.loc~eqtl.geno, main = "Expression in iPSCs")
-beeswarm(eqtl.loc~eqtl.geno, add=T, col="black", vertical=T,pch=20)
-
-
-dc.exprs = read.table("qqnorm.500ht.gccor.newcovcor.ordered.bimbam")
+#Load in dc data
+dc = read.table("qqnorm.500ht.gccor.newcovcor.ordered.bimbam")
 genes = read.table('qqnorm.500ht.gccor.newcovcor.genenames.txt', as.is=T)
-colnames(dc.exprs)=genes$V1
+colnames(dc)=genes$V1
+dc.exprs.2 = t(dc)
+d.pca = prcomp(dc, center = T,scale=T)
+#Regress out 62 PCs
+PC1_62.residual = matrix(nrow= nrow(dc.exprs.2), ncol = ncol(dc.exprs.2))
+rownames(PC1_62.residual) = rownames(dc.exprs.2)
+for (i in 1:nrow(dc.exprs.2)) {
+  model= lm(dc.exprs.2[i,]~ d.pca$x[,1:62])
+  PC1_62.residual[i,] = resid(model) #+model$coefficients[1]
+}
+
+dc.exprs = t(PC1_62.residual)
+
+#Load in dc geno
 geno.l = read.table('C:/Users/Courtney/Dropbox/LCL-iPSC/GEMMA eQTLs/Final Data/LCLall.eqtlgenotypes.raw', header=T, as.is=T, sep='\t')
 geno.l.t = t(geno.l)
 
+library(beeswarm)
+##Create pdfs
+pdf("Opposite_Direction_eQTLs.pdf")
+eqtl.loc = as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000164048")])
+eqtl.geno = as.matrix(geno.i.t[which(rownames(geno.i.t) == "rs12495221_C"),])
+boxplot(eqtl.loc~eqtl.geno, main = "ZNF589 Expression in iPSCs")
+beeswarm(eqtl.loc~eqtl.geno, add=T, col="black", vertical=T,pch=20)
+
+
 eqtl.loc = as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000164048")])
 eqtl.geno = as.matrix(geno.l.t[which(rownames(geno.l.t) == "rs12495221_C"),])
-boxplot(eqtl.loc~eqtl.geno, main = "Expression in LCLs")
+boxplot(eqtl.loc~eqtl.geno, main = "ZNF589 Expression in LCLs")
 beeswarm(eqtl.loc~eqtl.geno, add=T, col= "black", vertical=T,pch=20)
 
 
 eqtl.loc = as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000146833")])
 eqtl.geno = as.matrix(geno.i.t[which(rownames(geno.i.t) == "rs2571997_A"),])
-boxplot(eqtl.loc~eqtl.geno, main = "Expression in iPSCs")
+boxplot(eqtl.loc~eqtl.geno, main = "TRIM4 Expression in iPSCs")
 beeswarm(eqtl.loc~eqtl.geno, add=T, col="black", vertical=T,pch=20)
 
 eqtl.loc = as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000146833")])
 eqtl.geno = as.matrix(geno.l.t[which(rownames(geno.l.t) == "rs2571997_A"),])
-boxplot(eqtl.loc~eqtl.geno, main = "Expression in LCLs")
+boxplot(eqtl.loc~eqtl.geno, main = "TRIM4 Expression in LCLs")
 beeswarm(eqtl.loc~eqtl.geno, add=T, col= "black", vertical=T,pch=20)
 dev.off()
+
+#Most significant iPSC eQTL ENSG00000011405  rs17560341
+
+eqtl.loc = as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000011405")])
+eqtl.geno = as.matrix(geno.i.t[which(rownames(geno.i.t) == "rs17560341_A"),])
+boxplot(eqtl.loc~eqtl.geno, main = "Expression in iPSCs")
+beeswarm(eqtl.loc~eqtl.geno, add=T, col="black", vertical=T,pch=20)
+
+#Largest effect size eQTL ENSG00000143632  rs867325
+eqtl.loc = as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000143632")])
+eqtl.geno = as.matrix(geno.i.t[which(rownames(geno.i.t) == "rs867325_A"),])
+boxplot(eqtl.loc~eqtl.geno, main = "Expression in iPSCs")
+beeswarm(eqtl.loc~eqtl.geno, add=T, col="black", vertical=T,pch=20)
+
+
+boxplot(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000181449")]), main = "SOX2 Expression")
+boxplot(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000204531")]), main = "OCT3/4 Expression")
+boxplot(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000215271")]), main = "HOMEZ Expression")
+boxplot(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000187140")]), main = "FOXD3 Expression")
+var(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000187140")]))
+var(as.matrix(exprs.o.t[,which(colnames(exprs.o.t) == "ENSG00000215271")]))
+
+boxplot(as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000215271")]), main = "HOMEZ LCL Expression") 
+boxplot(as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000187140")]), main = "FOXD3 LCL Expression") 
+var(as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000215271")]))
+var(as.matrix(dc.exprs[,which(colnames(dc.exprs) == "ENSG00000187140")]))
